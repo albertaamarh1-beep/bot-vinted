26010e37d153839089ff332462356dc853405121
albertaamarh1-beep <albertaamarh1@gmail.com>
2026-03-31 20:13:59 +0200

    add flask for render

--- bot.py
+++ bot.py
@@ -18,4 +18,20 @@ async def ping(ctx):
     await ctx.send("Pong 🏓")
 
 bot.run(TOKEN)
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run).start()
+
+bot.run(TOKEN)
 
