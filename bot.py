9a763bfffc78ae292209463e6c66683e74fe47bb
albertaamarh1-beep <albertaamarh1@gmail.com>
2026-04-01 12:48:22 +0200

    fix bot loop

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from flask import Flask
 from threading import Thread
 
 TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = 1488540243266375877
 
 app = Flask(__name__)
 
@@ -17,34 +16,28 @@ def home():
 def run_web():
     app.run(host="0.0.0.0", port=10000)
 
-def keep_alive():
-    Thread(target=run_web).start()
+Thread(target=run_web).start()
 
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 
-async def vinted_loop():
-    print(">>> BOUCLE DÉMARRÉE <<<")
-    await bot.wait_until_ready()
+@bot.event
+async def on_ready():
+    print("BOT CONNECTÉ")
 
     while True:
-        print(">>> RECHERCHE VINTED <<<")
+        print("JE TOURNE")
+
         try:
             url = "https://www.vinted.fr/api/v2/catalog/items?search_text=nike"
             headers = {"User-Agent": "Mozilla/5.0"}
-            response = requests.get(url, headers=headers)
-            print("HTTP STATUS:", response.status_code)
+            r = requests.get(url, headers=headers)
+            print("STATUS:", r.status_code)
         except Exception as e:
             print("ERREUR:", e)
 
         await asyncio.sleep(20)
 
-@bot.event
-async def on_ready():
-    print(">>> BOT CONNECTÉ <<<")
-    bot.loop.create_task(vinted_loop())
+print("SCRIPT LANCÉ")
 
-print(">>> SCRIPT DÉMARRÉ <<<")
-
-keep_alive()
 bot.run(TOKEN)
